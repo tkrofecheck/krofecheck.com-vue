1116110d134d27dad5306fb8368c55cba06af655
Tim Krofecheck <tkrofecheck@gmail.com>
2021-07-30 16:47:04 -0400

    remove unused store

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import app from './app.store';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  modules: {
-    app,
-  },
+  modules: {},
 });
