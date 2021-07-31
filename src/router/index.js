e557fe2f084b1dbf3b790daf32b24363e8a39695
Tim Krofecheck <tkrofecheck@gmail.com>
2021-07-31 10:26:28 -0400

    another attempt to fix 404

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,12 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/views/Home.vue';
+import Portfolio from '@/views/Portfolio.vue';
+import NotFoundComponent from '@/views/NotFoundComponent.vue';
 
 Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '*',
-    beforeEnter: (to, from, next) => {
-      next('/404');
-    },
-  },
-
-  {
-    path: '/404',
-    name: '404',
-    component: () => import('@/views/NotFoundComponent'),
-  },
-
   {
     path: '/',
     name: 'Home',
@@ -27,11 +16,13 @@ const routes = [
   {
     path: '/portfolio',
     name: 'Portfolio',
-    // component: Portfolio,
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import('@/views/Portfolio.vue'),
+    component: Portfolio,
+  },
+
+  {
+    path: '*',
+    name: '404',
+    component: NotFoundComponent,
   },
 ];
 
