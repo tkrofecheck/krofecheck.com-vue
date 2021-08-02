c58e658828079577d57007e2a738dca5a074cca4
Tim Krofecheck <tkrofecheck@gmail.com>
2021-08-02 09:44:39 -0400

    updating dist for gh-pages

--- dist/js/404~f0676f14.js
+++ dist/js/404~f0676f14.js
@@ -1,9 +1,9 @@
 /*!
- * hash: e32ad3a2e09308aa3b66
+ * hash: 15061eb9615d3b0a52c8
  * name: 404~f0676f14
  * version: 1.1.3
  * file: js/404~f0676f14.js
- * timestamp: 2021-08-02T09:12:18.806-04:00
+ * timestamp: 2021-08-02T09:35:40.182-04:00
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["404~f0676f14"],{9703:function(t,o,n){"use strict";n.r(o);var e=function(){var t=this,o=t.$createElement,n=t._self._c||o;return n("center",[n("parallax",{attrs:{title:t.title,intro:!0}}),n("h3",[t._v(" Sorry you couldn't find what you were looking for. "),n("router-link",{attrs:{to:"/"}},[t._v("GO TO HOMEPAGE")])],1)],1)},a=[],r=n("035a"),l={name:"NotFoundComponent",components:{Parallax:r["a"]},data:function(){return{title:"Page Not Found - 404",info:'Sorry you couldn\'t find what you were looking for. <a to="/">GO TO HOMEPAGE</a>'}}},u=l,i=n("2877"),c=Object(i["a"])(u,e,a,!1,null,"f09b7ae4",null);o["default"]=c.exports}}]);
 //# sourceMappingURL=404~f0676f14.js.map
--- dist/js/chunk-vendors~5c9ee5b2.js
+++ dist/js/chunk-vendors~5c9ee5b2.js
@@ -1,9 +1,9 @@
 /*!
- * hash: e32ad3a2e09308aa3b66
+ * hash: 15061eb9615d3b0a52c8
  * name: chunk-vendors~5c9ee5b2
  * version: 1.1.3
  * file: js/chunk-vendors~5c9ee5b2.js
- * timestamp: 2021-08-02T09:12:18.807-04:00
+ * timestamp: 2021-08-02T09:35:40.182-04:00
  */
 (window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-vendors~5c9ee5b2"],[]]);
 //# sourceMappingURL=chunk-vendors~5c9ee5b2.js.map
